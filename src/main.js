23e93f21ee32d63df183f68e8859a978eb8f8846
clw_work <clw_work@163.com>
2020-02-27 14:12:48 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+
+import './mock/index'
+// if (process.env.NODE_ENV) {
+// }
+
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,43 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Layout from '../layout/index'
+import Login from '../views/Login'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta: {title: '登录'}
   },
+  // {
+  //   path: '/about',
+  //   component: () => import('../views/About.vue')
+  // },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/',
+    component: Layout,
+    children: [
+      { 
+        path: '/welcome/welcome',
+        component: () => import('../views/welcome/Welcome.vue') 
+      }, {
+        path: '/welcome/about',
+        component: () => import('../views/welcome/About.vue')
+      }, {
+        path: '/dashboard/console',
+        component: () => import('../views/dashboard/Console.vue')
+      }, { 
+        path: '/form/simple',
+        component: () => import('../views/form/Simple.vue') 
+      }, {
+        path: '/form/step',
+        component: () => import('../views/form/Step.vue')
+      }
+    ]
+  },
 ]
 
 const router = new VueRouter({
